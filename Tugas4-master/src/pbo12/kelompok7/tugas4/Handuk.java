30e43df333f387ac5a2bc0830742c37a39a62b47
ridwan0797 <32489960+ridwan0797@users.noreply.github.com>
2017-12-03 17:16:55 +0700

    Add files via upload

---
+++ Tugas4-master/src/pbo12/kelompok7/tugas4/Handuk.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo12.kelompok7.tugas4;
+
+/**
+ *
+ * @author JARVIS PC
+ */
+public class Handuk {
+        private String Handuk;
+
+    public String getHanduk() {
+        return Handuk;
+    }
+
+    public void setHanduk(String Handuk) {
+        this.Handuk = Handuk;
+    }
+    
+        
+
+    
+}
---
+++ Tugas4-master/src/pbo12/kelompok7/tugas4/PaketFitness.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo12.kelompok7.tugas4;
+
+
+/**
+ *
+ * @author JARVIS PC
+ */
+public class PaketFitness {
+    private String paket;
+    private int biaya; 
+    
+
+    public String getPaket() {
+        return paket;
+    }
+
+    public void setPaket(String paket) {
+        this.paket = paket;
+    }
+
+    public int getBiaya() {
+        return biaya;
+    }
+
+    public void setBiaya(int biaya) {
+        this.biaya = biaya;
+    }
+    
+}
